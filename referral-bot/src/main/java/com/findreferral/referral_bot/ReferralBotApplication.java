af6b74c22971a340f12c6ab32be8ae82e5ccf299
Nurstan Duisengaliyev <NurstanDuisengaliev@gmail.com>
2025-08-29 16:26:46 +0500

    Initial Commit

---
+++ referral-bot/src/main/java/com/findreferral/referral_bot/ReferralBotApplication.java
@@ -0,0 +1,13 @@
+package com.findreferral.referral_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferralBotApplication.class, args);
+	}
+
+}
---
+++ referral-bot/src/test/java/com/findreferral/referral_bot/ReferralBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findreferral.referral_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferralBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
